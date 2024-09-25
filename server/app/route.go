95a39cecf8c54442bc6783530575af3e78f37f9d
vicentdev <vicent@vicentdev.com>
2024-09-25 23:13:59 +0200

    refactor move consumers and handlers to independent files.

--- server/app/route.go
+++ server/app/route.go
@@ -1,14 +1,5 @@
 package app
 
-import (
-	"log"
-	"net/http"
-	"notice-me-server/pkg/notification"
-	"notice-me-server/pkg/rabbit"
-	"notice-me-server/pkg/repository"
-	"notice-me-server/pkg/websocket"
-)
-
 func (s *server) routes() {
 
 	s.r.Use(loggingMiddleware)
@@ -23,31 +14,3 @@ func (s *server) routes() {
 }
 
 // handlers @todo move if needed
-func (s *server) createNotificationHandler() func(w http.ResponseWriter, r *http.Request) {
-	rab := rabbit.NewRabbit(s.amqp, s.c.Rabbit.Queues)
-	repo := repository.GetRepository[notification.Notification](s.db)
-
-	return func(w http.ResponseWriter, r *http.Request) {
-		notification.CreateNotification(repo, rab)
-	}
-}
-
-func (s *server) wsHandler() func(w http.ResponseWriter, r *http.Request) {
-	ws := s.ws
-
-	return func(w http.ResponseWriter, r *http.Request) {
-
-		websocket.Upgrader.CheckOrigin = func(r *http.Request) bool { return true }
-
-		conn, err := websocket.Upgrader.Upgrade(w, r, nil)
-
-		if err != nil {
-			log.Println(err)
-			return
-		}
-		client := &websocket.Client{WebsocketService: ws, Conn: conn, Send: make(chan []byte, 256)}
-		client.WebsocketService.Register <- client
-
-		go client.Write()
-	}
-}
