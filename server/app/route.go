750bca213c2bd37d17d03881a9f8610d48cf9699
vicentdev <vicent@vicentdev.com>
2024-09-25 20:33:36 +0200

    wip refactor circular dependencies

--- server/app/route.go
+++ server/app/route.go
@@ -3,45 +3,47 @@ package app
 import (
 	"log"
 	"net/http"
-	"time"
+	"notice-me-server/app/rabbit"
+	"notice-me-server/app/websocket"
+	"notice-me-server/pkg/notification"
 )
 
-func (s *Server) routes() {
-
-	s.r.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
-		log.Println(r.URL)
-		if r.URL.Path != "/" {
-			http.Error(w, "Not found", http.StatusNotFound)
-			return
-		}
-		if r.Method != http.MethodGet {
-			http.Error(w, "Method not allowed", http.StatusMethodNotAllowed)
-			return
-		}
-		http.ServeFile(w, r, "../client/index.html")
-	})
+func (s *server) routes() {
+	apiRouter := s.r.PathPrefix("/api").Subrouter()
+	apiRouter.Use(jsonMiddleware)
 
 	s.r.Use(loggingMiddleware)
 
 	s.r.HandleFunc("/ws", s.wsHandler()).Methods("GET")
-	s.r.HandleFunc("/ping", func(w http.ResponseWriter, r *http.Request) {
-		s.broadcastAllClients([]byte("test " + time.Now().String()))
-	}).Methods("GET")
+
+	//notify POST
+	apiRouter.HandleFunc("/notification", s.createNotificationHandler()).Methods("POST")
 }
 
 // handlers @todo move if needed
-func (s *Server) wsHandler() func(w http.ResponseWriter, r *http.Request) {
+func (s *server) createNotificationHandler() func(w http.ResponseWriter, r *http.Request) {
+	rab := rabbit.NewRabbit(s.amqp, s.c.Rabbit.Queues)
+	return func(w http.ResponseWriter, r *http.Request) {
+		notification.CreateNotification(rab)
+	}
+}
+
+func (s *server) wsHandler() func(w http.ResponseWriter, r *http.Request) {
 	ws := s.ws
 
 	return func(w http.ResponseWriter, r *http.Request) {
-		conn, err := upgrader.Upgrade(w, r, nil)
+
+		websocket.Upgrader.CheckOrigin = func(r *http.Request) bool { return true }
+
+		conn, err := websocket.Upgrader.Upgrade(w, r, nil)
+
 		if err != nil {
 			log.Println(err)
 			return
 		}
-		client := &client{websocketService: ws, conn: conn, send: make(chan []byte, 256)}
-		client.websocketService.register <- client
+		client := &websocket.Client{WebsocketService: ws, Conn: conn, Send: make(chan []byte, 256)}
+		client.WebsocketService.Register <- client
 
-		go client.writePump()
+		go client.Write()
 	}
 }
--- server/app/server.go
+++ server/app/server.go
@@ -4,48 +4,53 @@ import (
 	"encoding/json"
 	"github.com/gorilla/handlers"
 	"github.com/gorilla/mux"
+	amqp "github.com/rabbitmq/amqp091-go"
+	"gorm.io/gorm"
 	"net/http"
+	"notice-me-server/app/config"
+	"notice-me-server/app/websocket"
 )
 
-type Server struct {
-	r      *mux.Router
-	ws     *hub
-	rabbit *rabbit
-	db     *db
-	c      *config
+type server struct {
+	r    *mux.Router
+	ws   *websocket.Hub
+	amqp *amqp.Connection
+	db   *gorm.DB
+	c    *config.Config
 }
 
-func NewServer() *Server {
-	s := Server{
-		c:  loadConfig(),
-		ws: newHub(),
+func NewServer() *server {
+	s := server{
+		c:  config.LoadConfig(),
+		ws: websocket.NewHub(),
 		r:  mux.NewRouter(),
 	}
 
-	s.db = s.newDb()
-	s.rabbit = s.newRabbit()
+	s.connectDb()
+	s.connectAmqp()
+
 	s.routes()
 
 	return &s
 }
 
-func (s *Server) Run() error {
+func (s *server) Run() error {
 
-	go func(websocket *hub) {
-		websocket.run()
+	go func(websocket *websocket.Hub) {
+		websocket.Run()
 	}(s.ws)
 
 	return http.ListenAndServe(":"+s.c.Server.Port, handlers.RecoveryHandler()(s.r))
 }
 
-func (s *Server) writeResponse(w http.ResponseWriter, response map[string]interface{}) {
+func (s *server) writeResponse(w http.ResponseWriter, response map[string]interface{}) {
 	w.WriteHeader(http.StatusOK)
 
 	byteResponse, _ := json.Marshal(response)
 	_, _ = w.Write(byteResponse)
 }
 
-func (s *Server) writeErrorResponse(w http.ResponseWriter, response map[string]interface{}, errorCode int) {
+func (s *server) writeErrorResponse(w http.ResponseWriter, response map[string]interface{}, errorCode int) {
 	w.WriteHeader(errorCode)
 	byteResponse, _ := json.Marshal(response)
 	_, _ = w.Write(byteResponse)
