04d226cb58b9b3771517e904a501a20a6c2fafe7
vicentdev <vicent@vicentdev.com>
2024-09-28 15:57:25 +0200

    demo rework wip

---
+++ demo/pages/util/api.ts
@@ -0,0 +1,27 @@
+import axios from "axios";
+
+export function getApiBaseURL(): string {
+  let host = process.env.NEXT_PUBLIC_NOTICEME_API_HOST;
+  if (!host) {
+    host = window.location.hostname;
+  }
+
+  return `${window.location.protocol}//${host}:${process.env.NEXT_PUBLIC_NOTICEME_API_PORT}/api`;
+}
+
+export function getWebsocketBaseURL() {
+  let host = process.env.NEXT_PUBLIC_NOTICEME_API_HOST;
+  if (!host) {
+    host = window.location.hostname;
+  }
+
+  const protocol = window.location.protocol === 'https:' ? 'wss' : 'ws';
+
+  return `${protocol}//${host}:${process.env.NEXT_PUBLIC_NOTICEME_API_PORT}`;
+}
+
+export function api() {
+  return axios.create({
+    baseURL: getApiBaseURL(),
+  });
+}
