265a8e37677724546ed00242ca9d7a689afcbf24
vicentdev <vicent@vicentdev.com>
2024-09-29 08:39:16 +0200

    group id not * in demo

--- demo/src/App.js
+++ demo/src/App.js
@@ -7,21 +7,34 @@ import PublishNotificationForm from "./component/PublishNotificationForm";
 function App() {
   const clientId = crypto.randomUUID();
 
+  const groupIds = [
+    'group_1',
+    'group_2',
+    'group_3',
+    'group_4',
+    'group_5',
+    'group_6',
+    'group_7',
+    'group_8',
+  ];
+
+  var clientGroupId = groupIds[Math.floor(Math.random() * groupIds.length)];
+
   return (
-      <>
-        <Grid
-            container
-            direction="column"
-            alignItems="center"
-            justifyContent="center"
-        >
-          <Box mb={2}>
-            <h1>Noticeme</h1>
-          </Box>
-          <PublishNotificationForm clientId={clientId}/>
-        </Grid>
-        <NoticeMe clientId={clientId} clientGroupId={'*'}/>
-      </>
+    <>
+      <Grid
+        container
+        direction="column"
+        alignItems="center"
+        justifyContent="center"
+      >
+        <Box mb={2}>
+          <h1>Noticeme</h1>
+        </Box>
+        <PublishNotificationForm clientId={clientId} clientGroupId={clientGroupId}/>
+      </Grid>
+      <NoticeMe clientId={clientId} clientGroupId={clientGroupId}/>
+    </>
   );
 }
 
